c259bd3e4e44ccfae93286e00a7a5e7051a137b6
marimoria <honiism@gmail.com>
2025-07-23 17:12:12 +0700

    Changed outdir

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -30,7 +30,7 @@ export default defineConfig({
 			})
 		},
 		build: {
-			outDir: resolve('out'),
+			outDir: resolve('out/renderer'),
 			emptyOutDir: true,
 			sourcemap: false,
 			minify: 'esbuild'
